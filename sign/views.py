d8f0f617ade57d31374a9dcecaa842be239d6630
Bp-luffy <764442055@qq.com>
2020-03-30 00:02:15 +0800

    django and git

--- sign/views.py
+++ sign/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib import auth
-from sign.models import Event
+from sign.models import Event,Guest
 
 # Create your views here.
 def index(request):
@@ -40,3 +40,10 @@ def search_name(request):
     search_name=request.GET.get('name','')
     event_list=Event.objects.filter(name__icontains=search_name)
     return render(request,"event_manage.html",{'user':username,'events':event_list})
+
+#嘉宾管理
+@login_required
+def guest_manage(request):
+    username=request.session.get('user','')
+    guest_list=Guest.objects.all()
+    return render(request,"guest_manage.html",{"user":username,"guests":guest_list})
