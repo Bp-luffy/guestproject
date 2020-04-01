68b1d3d50b41f74b1c5b17f208472320b0b05091
Bp-luffy <764442055@qq.com>
2020-04-01 23:31:24 +0800

    django and git

--- sign/views.py
+++ sign/views.py
@@ -73,5 +73,29 @@ def guest_manage(request):
 #签到页面
 @login_required
 def sign_index(request,eid):
-    event=get_object_or_404(Event,id=eid)
+    event=get_object_or_404(Event,event_id=eid)
     return render(request,'sign_index.html',{'event':event})
+#签到动作
+@login_required
+def sign_index_action(request,eid):
+    event=get_object_or_404(Event,event_id=eid)
+    phone=request.POST.get('phone','')
+    result=Guest.objects.filter(phone=phone)
+    if not result:
+        return render(request,'sign_index.html',{'event':event,'hint':'event id or phone error.'})
+    result=Guest.objects.filter(phone=phone,event_id=eid)
+    if not result:
+        return  render(request,'sign_index.html',{'event':event,'hint':'event id or phone error.'})
+    result=Guest.objects.get(phone=phone,event_id=eid)
+    if result.sign:
+        return render(request,'sign_index.html',{'event':event,'hint':'user has sign in.'})
+    else:
+        Guest.objects.filter(phone=phone,event_id=eid).update(sign='1')
+        return render(request,'sign_index.html',{'event_id':event,'hint':'sign in success!','guest':result})
+
+#退出登录
+@login_required
+def logout(request):
+    auth.logout(request) #t退出登录
+    response=HttpResponseRedirect('/sign/index/')
+    return response
